75a1c6d3336537c9b5830cb03062cad8ed1b62ea
DShaw0004 <114005178+dshaw0004@users.noreply.github.com>
2024-03-29 21:56:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,10 +16,6 @@ let server = app.listen(port, () => {
 });
 
 const io = new Server(server, { cors: { origin: [
-  'http://localhost:5173',
-  "https://lchat.dsx2.repl.co/",
-  "https://lchatgroup.web.app",
-  "https://anonylchat.web.app/",
   "*"
 ] } });
 
